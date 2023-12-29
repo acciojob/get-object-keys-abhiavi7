afdb8e54c0ef342843bfa16c50950cefaba8b48e
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-29 19:50:46 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,13 @@
-//your JS code here. If required.
-document.addEventListener("DOMContentLoaded", function () {
-    // Your JavaScript code that needs to run after the DOM has loaded
-    console.log("Script loaded!");
-});
+// Create a student object with a 'name' property
+let student = {
+    name: "John"
+};
+
+// Add a property to the Object prototype
+Object.prototype.getKeys = function () {
+    return Object.keys(this);
+};
+
+// Example usage
+let keys = student.getKeys();
+console.log(keys); // Output: ['name']
