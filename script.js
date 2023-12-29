b43b29621c4044c436490a5ad34f4b889a981e01
abhiavi7 <118717274+abhiavi7@users.noreply.github.com>
2023-12-29 19:48:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,5 @@
 //your JS code here. If required.
+document.addEventListener("DOMContentLoaded", function () {
+    // Your JavaScript code that needs to run after the DOM has loaded
+    console.log("Script loaded!");
+});
